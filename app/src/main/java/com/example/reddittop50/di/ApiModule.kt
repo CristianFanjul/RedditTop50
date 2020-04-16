ce901d58388969123dd28590e49bd086049396c9
CristianFanjulWiFiesta <cristian.fanjul@momentoustech.com>
2020-04-16 01:10:16 -0300

    Create retrofit stuff with dagger and avoid singleton.

--- app/src/main/java/com/example/reddittop50/di/ApiModule.kt
+++ app/src/main/java/com/example/reddittop50/di/ApiModule.kt
@@ -1,14 +1,48 @@
 package com.example.reddittop50.di
 
 import com.example.reddittop50.retrofit.IApiClient
-import com.example.reddittop50.retrofit.RetrofitFactory
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 class ApiModule {
+
+    companion object {
+        const val BASE_URL = "https://www.reddit.com/"
+    }
+
     @Singleton
     @Provides
-    fun provideApiClient(): IApiClient = RetrofitFactory.articlesRetrofitService()
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .connectTimeout(1, TimeUnit.MINUTES)
+            .readTimeout(1, TimeUnit.MINUTES)
+            .writeTimeout(1, TimeUnit.MINUTES)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiClient(retrofit: Retrofit): IApiClient {
+        return retrofit.create(IApiClient::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder().also {
+            it.client(okHttpClient)
+        }.baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
